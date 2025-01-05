72c7a6d5e5976065ed45910b7670e9fce102e191
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-01-05 19:25:24 +0900

    상윤 25.01.05

--- src/pages/BoardPage.js
+++ src/pages/BoardPage.js
@@ -38,7 +38,7 @@ const BoardPage = () => {
     const fetchPosts = async () => {
       setIsLoading(true); // 로딩 시작
       try {
-        const response = await axiosInstance.get('https://bcefb2d9d162.ngrok.app/api/board/free', {
+        const response = await axiosInstance.get('https://cce1-2406-5900-10f0-c886-2035-dcac-532c-702.ngrok-free.app/api/board/free', {
           params: { page, size }, // 페이지와 사이즈를 쿼리 파라미터로 추가
           headers: {
             'ngrok-skip-browser-warning': 'true', // 경고 페이지를 우회하는 헤더 추가
@@ -67,7 +67,7 @@ const BoardPage = () => {
     // 좋아요 10개 이상 게시물 가져오기
     const fetchTopLikedPosts = async () => {
       try {
-        const response = await axiosInstance.get('https://bcefb2d9d162.ngrok.app/api/board/free/top-liked', {
+        const response = await axiosInstance.get('https://cce1-2406-5900-10f0-c886-2035-dcac-532c-702.ngrok-free.app/api/board/free/top-liked', {
           headers: {
             'ngrok-skip-browser-warning': 'true', // 경고 페이지를 우회하는 헤더 추가
           },
@@ -116,7 +116,7 @@ const BoardPage = () => {
     if (searchTerm.trim() !== '') {
       try {
         console.log(`검색어: ${searchTerm}`);
-        const response = await axiosInstance.get('https://bcefb2d9d162.ngrok.app/api/board/free', {
+        const response = await axiosInstance.get('https://cce1-2406-5900-10f0-c886-2035-dcac-532c-702.ngrok-free.app/api/board/free', {
           params: {
             searchKeyword: searchTerm, // 검색어 전달
             page: 0,
@@ -166,7 +166,7 @@ const BoardPage = () => {
         typeKeyword: '', // 필요 시 값 설정
       };
 
-      const response = await axiosInstance.get('https://bcefb2d9d162.ngrok.app/api/board/free/sort-by-likes', {
+      const response = await axiosInstance.get('https://cce1-2406-5900-10f0-c886-2035-dcac-532c-702.ngrok-free.app/api/board/free/sort-by-likes', {
         params,
         headers: {
           'ngrok-skip-browser-warning': 'true', // 필요 시 유지
