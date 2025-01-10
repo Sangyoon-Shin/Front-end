ce98f9aeed5e9106c90c95127d065b0ef9fd080c
psy1218 <141633727+psy1218@users.noreply.github.com>
2025-01-10 20:17:37 +0900

    01/10

--- src/pages/QuestionCode.js
+++ src/pages/QuestionCode.js
@@ -44,7 +44,6 @@ const QuestionCode = () => {
         }
     }, [id]);
 
-<<<<<<< HEAD
     const fetchPostData = async (postId) => {
         try {
             const response = await axios.get(`${BASE_URL}/coding/update/${postId}`, {
@@ -58,18 +57,6 @@ const QuestionCode = () => {
         } catch (error) {
             console.error('Error fetching post data:', error);
             alert('게시글 정보를 불러오는 데 실패했습니다.');
-=======
-    // 반응형 처리를 위한 useMediaQuery 사용
-    const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' });
-
-    const handleLanguageChange = (language) => {
-        setSelectedLanguage(language);
-    };
-
-    const handleImageChange = (e) => {
-        if (e.target.files.length > 0) {
-            setImage(e.target.files[0]);
->>>>>>> 7090cc74850f8c1e4b61bb14f85aafb4644e7e59
         }
     };
 
@@ -137,7 +124,6 @@ const QuestionCode = () => {
                         </button>
                     ))}
                 </div>
-<<<<<<< HEAD
             </div>
 
 
@@ -188,85 +174,6 @@ const QuestionCode = () => {
                 <button className={styles["submit-button"]} onClick={handleSubmit}>
                     {isEditing ? '수정' : '작성'}
                 </button>
-=======
-
-                {/* 폼 입력 부분 */}
-                <div className={`${styles.form} ${isDesktop ? styles.desktopForm : ''}`}>
-                    {/* 언어 선택 */}
-                    <div className={`${styles.languageSelectionContainer} ${isDesktop ? styles.desktopLanguageSelectionContainer : ''}`}>
-                        <label className={`${styles.languageSelectionLabel} ${isDesktop ? styles.desktopLanguageSelectionLabel : ''}`}>언어 선택</label>
-                        <div className={`${styles.languageSelection} ${isDesktop ? styles.desktopLanguageSelection : ''}`}>
-                            {['C/C++', 'C#', 'JAVA', 'Python', '기타'].map((language) => (
-                                <button
-                                    key={language}
-                                    className={`${styles.languageButton} ${selectedLanguage === language ? styles.activeLanguageButton : ''}`}
-                                    onClick={() => handleLanguageChange(language)}
-                                >
-                                    {language}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-
-
-                    {/* 제목 입력 */}
-                    <div className={`${styles.inputContainer} ${isDesktop ? styles.desktopInputContainer : ''}`}>
-                        <label className={`${styles.languageSelectionLabel} ${isDesktop ? styles.desktopLanguageSelectionLabel : ''}`}>제목</label>
-                        <input
-                            type="text"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            placeholder=""
-                            className={`${styles.inputField} ${isDesktop ? styles.desktopInputField : ''}`}
-                        />
-                    </div>
-
-                    {/* 해시태그 입력 */}
-                    <div className={`${styles.inputContainer} ${isDesktop ? styles.desktopInputContainer : ''}`}>
-                        <label className={`${styles.languageSelectionLabel} ${isDesktop ? styles.desktopLanguageSelectionLabel : ''}`}>해시태그</label>
-                        <input
-                            type="text"
-                            value={hashTag}
-                            onChange={(e) => setHashTag(e.target.value)}
-                            placeholder="#해시태그"
-                            className={`${styles.inputField} ${isDesktop ? styles.desktopInputField : ''}`}
-                        />
-                    </div>
-
-                    {/* 이미지 첨부 */}
-                    <div className={`${styles.inputContainer} ${isDesktop ? styles.desktopInputContainer : ''}`}>
-                        <label className={`${styles.languageSelectionLabel} ${isDesktop ? styles.desktopLanguageSelectionLabel : ''}`}>이미지 첨부</label>
-                        <div className={`${styles.imageUpload} ${isDesktop ? styles.desktopImageUpload : ''}`}>
-                            <label htmlFor="imageUploadInput">
-                                <img src={PlusIcon} alt="이미지 첨부" className={styles.plusIcon} />
-                            </label>
-                            <input
-                                id="imageUploadInput"
-                                type="file"
-                                accept="image/*"
-                                onChange={handleImageChange}
-                                className={styles.imageInput}
-                            />
-                        </div>
-                    </div>
-
-                    {/* 내용 입력 */}
-                    <div className={`${styles.inputContainer} ${isDesktop ? styles.desktopInputContainer : ''}`}>
-                        <label className={`${styles.languageSelectionLabel} ${isDesktop ? styles.desktopLanguageSelectionLabel : ''}`}>내용</label>
-                        <textarea
-                            value={content}
-                            onChange={(e) => setContent(e.target.value)}
-                            placeholder="내용을 입력하세요..."
-                            className={`${styles.textarea} ${isDesktop ? styles.desktopTextarea: ''}`}
-                        />
-                    </div>
-
-                    {/* 게시글 올리기 버튼 */}
-                    <button className={`${styles.submitButton} ${isDesktop ? styles.desktopSubmitButton: ''}`} onClick={handleSubmit}>
-                        게시글 올리기
-                    </button>
-                </div>
->>>>>>> 7090cc74850f8c1e4b61bb14f85aafb4644e7e59
             </div>
         </div>
     );
